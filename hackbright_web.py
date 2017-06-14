5a8b8b0157ced46e43cc829fa7437fdace2e6435
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-13 18:02:04 -0700

    working on showing list of grades on student profile

--- hackbright_web.py
+++ hackbright_web.py
@@ -15,10 +15,14 @@ def get_student():
 
     first, last, github = hackbright.get_student_by_github(github)
 
+    grades = hackbright.get_grades_by_github(github)
+
     html = render_template("student_info.html",
                            first=first,
                            last=last,
-                           github=github)
+                           github=github,
+                           # project_title=project_title,
+                           grades=grades)
 
     return html
 
